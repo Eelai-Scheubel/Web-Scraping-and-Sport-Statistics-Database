688bb5d25d9e7facc1b78e382dc501b4ee7a5864
LaurRenman <listrati.laurentiu@gmail.com>
2025-01-10 14:22:07 +0100

    Update England.R

--- England.R
+++ England.R
@@ -1,57 +1,43 @@
-# Installer les librairies
 library(worldfootballR)
 library(dplyr)
+library(magrittr)
 library(writexl)
 
-# Système de délai pour pas être bloquer par le site
-response <- httr::GET("https://fbref.com/en/comps/9/2023-2024/2023-2024-Premier-League-Stats")
-retry_after <- httr::headers(response)$`retry-after`
-print(retry_after)
-if (!is.null(retry_after))
-  Sys.sleep(as.numeric(retry_after))  # Wait according to the server's recommendation
+# Funcție pentru extragerea și procesarea unui tip de statistică
+process_stat <- function(team_urls, stat_type, filter_cols) {
+  stats <- fb_team_match_log_stats(team_urls = team_urls, stat_type = stat_type) %>%
+    filter(Comp == "Premier League", ForAgainst == "For") %>%
+    select(-all_of(filter_cols))
+  Sys.sleep(3) # Pauză între cereri
+  return(stats)
+}
 
+# URL-ul echipelor
+url <- fb_teams_urls("https://fbref.com/en/comps/9/2023-2024/2023-2024-Premier-League-Stats")
 
-# Url du championnat
-url_england <- fb_teams_urls("https://fbref.com/en/comps/9/2023-2024/2023-2024-Premier-League-Stats")
+# Specificații pentru tipurile de statistici și coloane de eliminat
+stat_types <- c("shooting", "passing", "keeper", "passing_types", "gca", "defense", "misc")
+cols_to_remove <- list(
+  shooting = c("Team_Url", "ForAgainst", "Date", "Time", "Comp", "Day", "Venue", "Result", "GA", "Gls_Standard"),
+  other_stats = c(1:13) # Coloane comune de eliminat
+)
 
+final_data_frames <- list()
 
-# Scrapping pour toutes les équipes du championnat
-for (i in 1:length(url_england)) {
-  shooting <- dplyr::filter(fb_team_match_log_stats(team_urls = url_england[i], stat_type = "shooting"), Comp == "Premier League", ForAgainst == "For")
-  shooting <- subset(shooting, select = -c(Team_Url, ForAgainst, Date, Time, Comp, Day, Venue, Result, GA, Gls_Standard))
-  Sys.sleep(3)
+# Iterăm pentru fiecare echipă
+for (i in seq_along(url)) {
+  team_url <- url[i]
   
-  passing <- dplyr::filter(fb_team_match_log_stats(team_urls = url_england[i], stat_type = "passing"), Comp == "Premier League", ForAgainst == "For")
-  passing <- subset(passing, select = -c(1:13))
-  Sys.sleep(3)
+  # Procesăm toate tipurile de statistici
+  stats_list <- lapply(stat_types, function(stat_type) {
+    filter_cols <- if (stat_type == "shooting") cols_to_remove$shooting else cols_to_remove$other_stats
+    process_stat(team_url, stat_type, filter_cols)
+  })
   
-  keeper <- dplyr::filter(fb_team_match_log_stats(team_urls = url_england[i], stat_type = "keeper"), Comp == "Premier League", ForAgainst == "For")
-  keeper <- subset(keeper, select = -c(1:13))
-  Sys.sleep(3)
-  
-  passing_types <- dplyr::filter(fb_team_match_log_stats(team_urls = url_england[i], stat_type = "passing_types"), Comp == "Premier League", ForAgainst == "For")
-  passing_types <- subset(passing_types, select = -c(1:13))
-  Sys.sleep(3)
-  
-  gca <-dplyr::filter(fb_team_match_log_stats(team_urls = url_england[i], stat_type = "gca"), Comp == "Premier League", ForAgainst == "For")
-  gca <- subset(gca, select = -c(1:13))
-  Sys.sleep(3)
-  
-  defense <- dplyr::filter(fb_team_match_log_stats(team_urls = url_england[i], stat_type = "defense"), Comp == "Premier League", ForAgainst == "For")
-  defense <- subset(defense, select = -c(1:13))
-  Sys.sleep(3)
-  
-  misc <- dplyr::filter(fb_team_match_log_stats(team_urls = url_england[i], stat_type = "misc"), Comp == "Premier League", ForAgainst == "For")
-  misc <- subset(misc, select = -c(1:13))
-  Sys.sleep(3)
-  
-  merged_df <- cbind(shooting, passing, keeper, passing_types, gca, defense, misc)
-  
+  # Combinăm statisticile pentru echipa curentă
+  merged_df <- do.call(cbind, stats_list)
   final_data_frames[[i]] <- merged_df
 }
 
-# Combiner les stats de toutes les équipes en une database
+# Combinăm toate echipele într-un singur dataframe
 combined_df <- bind_rows(final_data_frames)
-
-# Transformer en un fichier excel
-write_xlsx(final_df_england, "Database PL.xlsx")
