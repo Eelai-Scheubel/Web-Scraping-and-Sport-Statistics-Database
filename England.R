05a34225dbc6191f9ce73e8c69e42997ae324210
Eelai Scheubel <189846219+Eelai-Scheubel@users.noreply.github.com>
2025-01-11 16:46:10 +0100

    Update England.R

--- England.R
+++ England.R
@@ -1,45 +1,53 @@
+# Importation des librairies
 library(worldfootballR)
 library(dplyr)
 library(magrittr)
 library(writexl)
 
-# Fonction pour extraire et traiter un type de statistique
-process_stat <- function(team_urls, stat_type, filter_cols) {
+# Fonction pour extraire et filtrer les différentes catégories de statistiques (shooting, passing, etc...)
+filter_stat <- function(team_urls, stat_type, comp) {           # team_urls = lien de l'équipe, stat_type = catégorie de stat, comp = compétition
   stats <- fb_team_match_log_stats(team_urls = team_urls, stat_type = stat_type) %>%
-    filter(Comp == "Premier League", ForAgainst == "For") %>%
-    select(-all_of(filter_cols)) # Suppression des colonnes inutiles
-  Sys.sleep(3) # Pause entre les requêtes pour éviter une surcharge du serveur
+    filter(Comp == comp, ForAgainst == "For") %>%
+  
+    # Suppression des colonnes inutiles
+    select(-all_of(if (stat_type == "shooting") {
+      c("Team_Url", "ForAgainst", "Gls_Standard")
+    } else {
+      c(1:13)
+    }))
+  Sys.sleep(3) # Pause pour éviter de se faire bloquer par le site
   return(stats)
 }
 
-# URL des équipes
-url <- fb_teams_urls("https://fbref.com/en/comps/9/2023-2024/2023-2024-Premier-League-Stats")
-
-# Spécifications des types de statistiques et des colonnes à supprimer
-stat_types <- c("shooting", "passing", "keeper", "passing_types", "gca", "defense", "misc")
-cols_to_remove <- list(
-  shooting = c("Team_Url", "ForAgainst", "Date", "Time", "Comp", "Day", "Venue", "Result", "GA", "Gls_Standard"), # Colonnes spécifiques pour "shooting"
-  other_stats = c(1:13) # Colonnes communes à supprimer pour les autres statistiques
-)
-
-final_data_frames <- list()
-
-# Boucle sur chaque équipe
-for (i in seq_along(url)) {
-  team_url <- url[i]
-  
-  # Traitement de tous les types de statistiques
+# Fonction pour fusionner les statistiques d'une équipe
+combine_team_stats <- function(team_url, stat_types, comp) {
   stats_list <- lapply(stat_types, function(stat_type) {
-    filter_cols <- if (stat_type == "shooting") cols_to_remove$shooting else cols_to_remove$other_stats
-    process_stat(team_url, stat_type, filter_cols)
+    filter_stat(team_url, stat_type, comp)
   })
-  
-  # Combinaison des statistiques pour l'équipe actuelle
-  merged_df <- do.call(cbind, stats_list)
-  final_data_frames[[i]] <- merged_df
+  merged_df <- do.call(cbind, stats_list) # On fusionne les différentes catégories de statistiques d'une équipe en un seul dataframe
+  return(merged_df)
 }
 
-# Fusion de toutes les équipes en un seul dataframe
-combined_df <- bind_rows(final_data_frames)
+# Fonction principale
+fetch_premier_league_stats <- function(url, stat_types, comp) {
+  team_urls <- fb_teams_urls(url)
+  
+  # On fusionne les différentes catégories de statistiques pour toutes les équipes du championnat
+  final_data_frames <- lapply(team_urls, function(team_url) {
+    combine_team_stats(team_url, stat_types, comp) 
+  })
+  combined_df <- bind_rows(final_data_frames) # Fusion des stats de toutes les équipes
+  return(combined_df)
+}
+
+### Utilisation pour n'importe quelle championnat (ici la saison 2023-2024 de la Premier League)
+url <- "https://fbref.com/en/comps/9/2023-2024/2023-2024-Premier-League-Stats"  # Lien du championnat 
+stat_types <- c("shooting", "passing", "keeper", "passing_types", "gca", "defense", "misc") # Les catégories de statistiques qui nous intéressent
+comp <- "Premier League" # Nom de la compétition
+name_xl <- "Database_PL.xlsx"
+
+# Récupération des statistiques
+combined_df <- fetch_premier_league_stats(url, stat_types, comp)
+
 # Sauvegarde du résultat
-write_xlsx(combined_df, "Database PL.xlsx")
+write_xlsx(combined_df, name_xl)
